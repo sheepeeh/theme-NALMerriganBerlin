cc2fc25fc32995794edfb24352b6b48d1619f373
Rachel Donahue <donahrm@gmail.com>
2014-08-11 14:37:09 -0400

    remove breadcrumbs :bread:

--- simple-pages/page/show.php
+++ simple-pages/page/show.php
@@ -14,7 +14,6 @@
             }
         ?>
     <?php else: ?>
-        <p id='simple-pages-breadcrumbs'><?php echo simple_pages_display_breadcrumbs(); ?></p>
         <h1><?php echo metadata('simple_pages_page', 'title'); ?></h1>
         <?php
         $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
