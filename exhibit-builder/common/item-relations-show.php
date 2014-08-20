6d2606d18c5e3f994edcfc79b2e839bbf4c0bf96
Rachel Donahue <donahrm@gmail.com>
2014-08-20 12:33:52 -0400

    copy to eb

--- exhibit-builder/common/item-relations-show.php
+++ exhibit-builder/common/item-relations-show.php
@@ -5,12 +5,29 @@
     <?php else: ?>
 
         <h3>Related Items</h3>
+        
+        <?php  
+        //Check to see if the relationship text is "Has Part", then output appropriate text.
+            if ($subjectRelations){
+                $keys = array_keys($subjectRelations); 
+                $member = $subjectRelations[$keys[1]];
+                $keys2 = array_keys($member);
+                $relText = $member[$keys2[3]];
+                
+                if ($relText == 'Has Part') {
+                    echo "<p style='margin-top:0px;font-style:italic;'>The following items were originally housed in this binder.</p>";
+                }
+            }
+        ?>
 
+            
+        
         <?php foreach ($subjectRelations as $subjectRelation): ?>
             <div class="element-text">
                <?php if ($subjectRelation['relation_text'] == "Has Part" && $subjectRelation['object_item_id'] != metadata('item','id')): ?>
+                
                 <ul>
-                    <li><a href="<?php echo url('items/show/' . $subjectRelation['object_item_id']); ?>"><?php echo $subjectRelation['object_item_title']; ?></a> was originally a part of this item.</li>
+                    <li class="binder-items"><a href="<?php echo url('items/show/' . $subjectRelation['object_item_id']); ?>"><?php echo $subjectRelation['object_item_title']; ?></a></li>
                 </ul>
 
                 <?php else: ?>
