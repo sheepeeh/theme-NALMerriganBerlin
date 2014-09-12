8653caf278a4c6fc5ecf5092a794becb048ad227
Rachel Donahue <donahrm@gmail.com>
2014-09-12 16:33:50 -0400

    add shortcode support

--- simple-pages/page/show.php
+++ simple-pages/page/show.php
@@ -16,12 +16,8 @@
     <?php else: ?>
         <h1><?php echo metadata('simple_pages_page', 'title'); ?></h1>
         <?php
-        $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
-        if (metadata('simple_pages_page', 'use_tiny_mce')) {
-            echo $text;
-        } else {
-            echo eval('?>' . $text);
-        }
+            $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
+            echo $this->shortcodes($text);
         ?>
     <?php endif; ?>
 </div>
